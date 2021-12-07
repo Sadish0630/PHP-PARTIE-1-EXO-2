463c47bdd931d55d44453e79cf2ca9a5de3bec4a
audemar <audemarsadish@gmail.com>
2021-12-07 13:33:15 +0100

    exo2 terminé

---
+++ exo2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css"/>
+    <title>PARTIE 1 EXO 2</title>
+</head>
+<body>
+    <p>
+   <?php
+   $lastname = 'Audemar';
+   $firstname = 'Sadish';
+   $age = '40';
+   echo 'Bonjour,'." ".$lastname." ".$firstname." ".'a'." ".$age." ".'ans.' ;
+   ?>
+   </p>
+</body>
+</html>
